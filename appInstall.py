5492e21bc222ef73182f75f6b5717a1cad73ad38
Levente Rozina <77845318+HeldLake@users.noreply.github.com>
2022-12-18 10:46:19 +0100

    Add files via upload

---
+++ appInstall.py
@@ -0,0 +1,13 @@
+import os
+##os.system("sudo su")
+os.system("sudo apt-get update")
+os.system("sudo apt install git")
+os.system("sudo apt install vlc")
+os.system("sudo apt install vim")
+os.system("sudo apt install transmission")
+os.system("sudo apt install tlp")
+os.system("sudo apt install cmatrix")
+os.system("sudo apt install caffeine")
+os.system("sudo apt install python3-pip")
+os.system("sudo apt install update")
+
