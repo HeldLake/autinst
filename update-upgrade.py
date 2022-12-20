45804c5428f62778548bb53a1c9794903c0ea5b8
Levente Rozina <77845318+HeldLake@users.noreply.github.com>
2022-12-20 21:16:23 +0100

    Delete update-upgrade.py

--- update-upgrade.py
+++
@@ -1,6 +0,0 @@
-import os
-##os.system("sudo apt-get update")
-os.system("sudo apt-get upgrade")
-os.system("sudo apt-get update")
-os.system("poweroff") ##delete this line if you don't want your pc to shutdown after the update finishes
-
